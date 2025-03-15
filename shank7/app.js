5b99eefedd6f1b2721954c92b9bcac825a464beb
Mohammed Hatem <mr.mohammedhatem@gmail.com>
2025-03-15 02:07:10 +0200

    Fix filename casing issues

---
+++ shank7/app.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Shank7 fooo</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
